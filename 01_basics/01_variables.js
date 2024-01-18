2ab2d4e19a8cd078b6d8bd4cc793b06d1e1e2bfb
Rahul Yadav <ydv.rahul3108@gmail.com>
2024-01-18 10:24:58 +0000

    Datatypes in JS

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const userId = 1233
+let userEmail = "rahul@gmail.com";
+var userPassword = "3214";
+userCity = "Udaipur";
+
+// userId = 3231 // This is wrong because we cannot change const value
+
+// Avoid var - because it has issues with block scope and functional scope
+
+console.table([userId, userEmail , userPassword , userCity]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treating all JS code as newer version
+
+// alert(3 + 2); // this is NodeJS not browser - so it wont work
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Rahul")
+
+
+let name = "Rahul"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint => for bigger numbers like in Stock , Reddit
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
